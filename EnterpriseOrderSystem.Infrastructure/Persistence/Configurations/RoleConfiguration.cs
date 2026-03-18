615039fac81d8596158bdb6676a14f78407db45b
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-18 14:40:34 +0700

    Configured Role entity mapping

---
+++ EnterpriseOrderSystem.Infrastructure/Persistence/Configurations/RoleConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using EnterpriseOrderSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseOrderSystem.Infrastructure.Persistence.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.ToTable("Roles");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
