509931653fa0a3dfdb1cd15abba5ded4bcb5cbe6
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-18 13:44:23 +0700

    Configured Order entity mapping

---
+++ EnterpriseOrderSystem.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using EnterpriseOrderSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseOrderSystem.Infrastructure.Persistence.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.TotalAmount)
+                .HasPrecision(18, 2);
+
+            builder.HasMany(typeof(OrderItem), "_items")
+                .WithOne()
+                .HasForeignKey("OrderId");
+        }
+    }
+}
