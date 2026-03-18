b72c55469d9431d8add9332ab3b3827f5f7befac
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-18 16:30:46 +0700

    Implemented JWT token generation service

--- EnterpriseOrderSystem.Infrastructure/Persistence/Services/JwtService.cs
+++ EnterpriseOrderSystem.Infrastructure/Persistence/Services/JwtService.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using EnterpriseOrderSystem.Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EnterpriseOrderSystem.Infrastructure.Persistence.Services
 {
-    class JwtService
+    public class JwtService : IJwtService
     {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(Guid userId, string email, string role)
+        {
+            var jwtSettings = _configuration.GetSection("Jwt");
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(jwtSettings["Key"]!));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+            new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
+            new Claim(ClaimTypes.Email, email),
+            new Claim(ClaimTypes.Role, role)
+        };
+
+            var token = new JwtSecurityToken(
+                issuer: jwtSettings["Issuer"],
+                audience: jwtSettings["Audience"],
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(int.Parse(jwtSettings["ExpiryMinutes"]!)),
+                signingCredentials: credentials
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
