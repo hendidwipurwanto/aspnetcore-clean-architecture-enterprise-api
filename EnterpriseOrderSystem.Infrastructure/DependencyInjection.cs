6d6fe4f376e3eee467b117ab264960dd549326f2
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-18 15:32:08 +0700

    Added DependencyInjection and rRegister it into Program.cs

--- EnterpriseOrderSystem.Infrastructure/DependencyInjection.cs
+++ EnterpriseOrderSystem.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using EnterpriseOrderSystem.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,14 @@ using System.Threading.Tasks;
 
 namespace EnterpriseOrderSystem.Infrastructure
 {
-    class DependencyInjection
+    public static class DependencyInjection
     {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            return services;
+        }
     }
 }
