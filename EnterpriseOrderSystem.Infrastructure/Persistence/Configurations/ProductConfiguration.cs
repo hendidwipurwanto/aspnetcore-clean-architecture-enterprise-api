f26f9f37310b1db802386452e208ea034cd0c431
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-18 13:40:15 +0700

    configure Product entity mapping

---
+++ EnterpriseOrderSystem.Infrastructure/Persistence/Configurations/ProductConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using EnterpriseOrderSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseOrderSystem.Infrastructure.Persistence.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.ToTable("Products");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Price)
+                .HasPrecision(18, 2);
+
+            builder.Property(x => x.Stock)
+                .IsRequired();
+        }
+    }
+
+}
