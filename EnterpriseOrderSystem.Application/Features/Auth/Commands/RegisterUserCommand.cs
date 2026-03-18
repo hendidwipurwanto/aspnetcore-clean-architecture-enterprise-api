52c3610aee6c808b3ab4a9602301edb1b7e07690
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-18 17:17:11 +0700

    Setup folder command, handler and DtOs of Auth

---
+++ EnterpriseOrderSystem.Application/Features/Auth/Commands/RegisterUserCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseOrderSystem.Application.Features.Auth.Commands
+{
+    class RegisterUserCommand
+    {
+    }
+}
