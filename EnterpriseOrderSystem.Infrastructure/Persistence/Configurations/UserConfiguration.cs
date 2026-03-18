eba84b133668dd8aec256f1df5496f9a9efc2100
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-18 13:39:02 +0700

    Configured User entity mapping

---
+++ EnterpriseOrderSystem.Infrastructure/Persistence/Configurations/UserConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using EnterpriseOrderSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseOrderSystem.Infrastructure.Persistence.Configurations
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.ToTable("Users");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Email)
+                .IsRequired()
+                .HasMaxLength(200);
+
+            builder.Property(x => x.PasswordHash)
+                .IsRequired();
+
+            builder.HasOne(x => x.Role)
+                .WithMany()
+                .HasForeignKey(x => x.RoleId);
+        }
+    }
+}
