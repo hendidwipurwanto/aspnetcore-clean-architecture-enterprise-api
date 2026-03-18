7dcb0801d9b6421434e4aa0512622e7ba877cc04
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-18 16:38:52 +0700

    Registered JWT service in dependency injection

--- EnterpriseOrderSystem.Infrastructure/DependencyInjection.cs
+++ EnterpriseOrderSystem.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,6 @@
-﻿using EnterpriseOrderSystem.Infrastructure.Persistence;
+﻿using EnterpriseOrderSystem.Application.Interfaces;
+using EnterpriseOrderSystem.Infrastructure.Persistence;
+using EnterpriseOrderSystem.Infrastructure.Persistence.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,6 +18,8 @@ namespace EnterpriseOrderSystem.Infrastructure
         {
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IJwtService, JwtService>();
 
             return services;
         }
