8c524c3cd8d919b3e64338350fc4e8815e0ac446
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-18 14:39:19 +0700

    Configured OrderItem entity mapping

---
+++ EnterpriseOrderSystem.Infrastructure/Persistence/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using EnterpriseOrderSystem.Domain.Entities;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseOrderSystem.Infrastructure.Persistence.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            builder.ToTable("OrderItems");
+
+            builder.HasKey("Id");
+
+            builder.Property(x => x.Price)
+                .HasPrecision(18, 2);
+
+            builder.Property(x => x.Quantity)
+                .IsRequired();
+        }
+    }
+}
