f488b7dafaa544fa2085673c70941434d1861da6
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-19 22:57:46 +0700

    Added GlobalExceptionMiddleware

---
+++ EnterpriseOrderSystem.API/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace EnterpriseOrderSystem.API.Middlewares
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var statusCode = ex switch
+            {
+                UnauthorizedAccessException => HttpStatusCode.Unauthorized,
+                KeyNotFoundException => HttpStatusCode.NotFound,
+                ArgumentException => HttpStatusCode.BadRequest,
+                _ => HttpStatusCode.InternalServerError
+            };
+
+            var response = new
+            {
+                success = false,
+                message = ex.Message,
+                errors = (object?)null
+            };
+
+            var json = JsonSerializer.Serialize(response);
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)statusCode;
+
+            return context.Response.WriteAsync(json);
+        }
+    }
+}
