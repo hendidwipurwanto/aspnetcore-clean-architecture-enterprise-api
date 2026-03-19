ecd02aa78daa8135dbb917fdbabca3b55cf261a0
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-19 10:18:22 +0700

    Configured Swagger with bearer token

--- EnterpriseOrderSystem.API/Program.cs
+++ EnterpriseOrderSystem.API/Program.cs
@@ -5,6 +5,7 @@ using EnterpriseOrderSystem.Application.Common;
 using System.Text;
 using EnterpriseOrderSystem.Application;
 using EnterpriseOrderSystem.Domain.Entities;
+using Microsoft.OpenApi.Models;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,7 +14,33 @@ builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Masukkan token dengan format: Bearer {your token}"
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            new string[] {}
+        }
+    });
+});
 
 var jwtSettings = builder.Configuration.GetSection("Jwt");
 
