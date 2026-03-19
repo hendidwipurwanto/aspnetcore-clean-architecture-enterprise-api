aeb73429c56c009196583ad1eb2e040fc698e8cd
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-19 09:47:54 +0700

    Added LoginUserCommand and LoginUserHandler

--- EnterpriseOrderSystem.Application/Features/Auth/Commands/LoginUserCommand.cs
+++ EnterpriseOrderSystem.Application/Features/Auth/Commands/LoginUserCommand.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EnterpriseOrderSystem.Application.Features.Auth.DTOs;
+using MediatR;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace EnterpriseOrderSystem.Application.Features.Auth.Commands
 {
-    class LoginUserCommand
-    {
-    }
+    public record LoginUserCommand(
+      string Email,
+      string Password
+  ) : IRequest<AuthResponseDto>;
 }
--- EnterpriseOrderSystem.Application/Features/Auth/Handlers/LoginUserHandler.cs
+++ EnterpriseOrderSystem.Application/Features/Auth/Handlers/LoginUserHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using EnterpriseOrderSystem.Application.Features.Auth.Commands;
+using EnterpriseOrderSystem.Application.Features.Auth.DTOs;
+using EnterpriseOrderSystem.Application.Interfaces;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,37 @@ using System.Threading.Tasks;
 
 namespace EnterpriseOrderSystem.Application.Features.Auth.Handlers
 {
-    class LoginUserHandler
+    public class LoginUserHandler : IRequestHandler<LoginUserCommand, AuthResponseDto>
     {
+        private readonly ApplicationDbContext _context;
+        private readonly IJwtService _jwtService;
+
+        public LoginUserHandler(ApplicationDbContext context, IJwtService jwtService)
+        {
+            _context = context;
+            _jwtService = jwtService;
+        }
+
+        public async Task<AuthResponseDto> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await _context.Users
+                .Include(x => x.Role)
+                .FirstOrDefaultAsync(x => x.Email == request.Email, cancellationToken);
+
+            Console.WriteLine(user.PasswordHash); // hashed
+            Console.WriteLine(request.Password); // plain
+
+            if (user == null || !BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
+                throw new Exception("Invalid credentials");
+
+            var token = _jwtService.GenerateToken(user.Id, user.Email, user.Role.Name);
+
+            return new AuthResponseDto
+            {
+                Token = token,
+                Email = user.Email,
+                Role = user.Role.Name
+            };
+        }
     }
 }
