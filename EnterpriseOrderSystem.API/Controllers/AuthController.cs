9b475295963e3debe8cbe3f9ae1baf79aaae9ec3
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-19 15:08:26 +0700

    Added Refresh Action token in AuthController

--- EnterpriseOrderSystem.API/Controllers/AuthController.cs
+++ EnterpriseOrderSystem.API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using EnterpriseOrderSystem.Application.Features.Auth.Commands;
+using EnterpriseOrderSystem.Application.Features.Auth.DTOs;
 
 namespace EnterpriseOrderSystem.API.Controllers
 {
@@ -26,6 +27,17 @@ namespace EnterpriseOrderSystem.API.Controllers
         public async Task<IActionResult> Login([FromBody] LoginUserCommand command)
         {
             var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPost("refresh")]
+        public async Task<IActionResult> Refresh([FromBody] RefreshTokenRequestDto request)
+        {
+            var result = await _mediator.Send(new RefreshTokenCommand
+            {
+                RefreshToken = request.RefreshToken
+            });
+
             return Ok(result);
         }
     }
