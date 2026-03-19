dd4c6513dd186b3de78c062069ced81d26c3d7b3
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-19 09:47:09 +0700

    Added uth and Products Controller

---
+++ EnterpriseOrderSystem.API/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using EnterpriseOrderSystem.Application.Features.Auth.Commands;
+
+namespace EnterpriseOrderSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginUserCommand command)
+        {
+            var result = await _mediator.Send(command);
+            return Ok(result);
+        }
+    }
+}
---
+++ EnterpriseOrderSystem.API/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace EnterpriseOrderSystem.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+
+        [HttpGet("public")]
+        public IActionResult Public()
+        {
+            return Ok("This is public endpoint");
+        }
+
+
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetProducts()
+        {
+            return Ok("You are authenticated!");
+        }
+
+
+        [HttpPost]
+        [Authorize(Roles = "Admin")]
+        public IActionResult CreateProduct()
+        {
+            return Ok("Only admin can create product");
+        }
+
+        // 👤 GET CURRENT USER FROM TOKEN
+        [HttpGet("me")]
+        [Authorize]
+        public IActionResult GetCurrentUser()
+        {
+            var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var email = User.FindFirst(ClaimTypes.Email)?.Value;
+            var role = User.FindFirst(ClaimTypes.Role)?.Value;
+
+            return Ok(new
+            {
+                userId,
+                email,
+                role
+            });
+        }
+    }
+}
