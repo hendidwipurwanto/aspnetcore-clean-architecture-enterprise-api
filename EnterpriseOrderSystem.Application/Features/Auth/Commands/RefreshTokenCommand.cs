092285848ccd939cb7e361006eb901ce35ee1224
Hendi Dwi Purwanto <hendi.dp@gmail.com>
2026-03-19 15:06:27 +0700

    Added RefreshTokenCommand

---
+++ EnterpriseOrderSystem.Application/Features/Auth/Commands/RefreshTokenCommand.cs
@@ -0,0 +1,15 @@
+﻿using EnterpriseOrderSystem.Application.Features.Auth.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnterpriseOrderSystem.Application.Features.Auth.Commands
+{
+    public class RefreshTokenCommand : IRequest<AuthResponseDto>
+    {
+        public string RefreshToken { get; set; }
+    }
+}
